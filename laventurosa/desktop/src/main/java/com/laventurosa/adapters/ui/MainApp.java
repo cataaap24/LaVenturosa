adabb154651abd05822ce97a6e98aaf3bac691e2
c.pineda <c.pineda@unillanos.edu.co>
2026-05-04 12:51:58 -0500

    Initialize JavaFX application in MainApp class

--- laventurosa/desktop/src/main/java/com/laventurosa/adapters/ui/MainApp.java
+++ laventurosa/desktop/src/main/java/com/laventurosa/adapters/ui/MainApp.java
@@ -1,1 +1,11 @@
+package com.laventurosa.adapters.ui;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.application.Platform;
+
+public class MainApp extends Application {
+   
+    public static void main(String[] args) { launch(args); }
+}
